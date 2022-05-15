0162dead435076855d3514b04be0753ac0e707af
GunaWei0228 <zx910228@gmail.com>
2022-05-15 11:50:52 +0800

    v.1

---
+++ app/src/main/java/tw/edu/pu/midtermgw0515/MainActivity.kt
@@ -0,0 +1,12 @@
+package tw.edu.pu.midtermgw0515
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
